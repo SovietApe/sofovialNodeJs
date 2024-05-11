38f93564c1a1709958f85989d1e4f32b934a2b38
Lautaro Alegria <guidospano562@gmai.com>
2024-05-11 18:46:14 -0300

    first commit

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use('/videos', useVideos)
 app.use('/playlists', usePlaylists)
 app.use('/search', useSearch)
 
-schedule.scheduleJob('45 * * * *', function(){
+schedule.scheduleJob('55 * * * *', function(){
   youtubeVideosRefresh()
 })
 
